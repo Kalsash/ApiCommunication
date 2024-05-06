92edcd7f0aef6728e01e5590a34ce7504dfe25b5
Aleksandr Kalinin <akalinin@sfedu.ru>
2024-05-06 19:27:45 +0300

    Добавьте файлы проекта.

---
+++ UsersApi/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UsersApi.Models
+{
+	[Table("users")]
+	public class User
+	{
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Column("name")]
+		public string? Name { get; set; }
+
+		[Column("phone")]
+		public string? Phone { get; set; }
+
+		[Column("address")]
+		public string Address { get; set; }
+	}
+}
---
+++ UsersApi/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using UsersApi.Models;
+using UsersApiData;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<UserContext>(options =>
+	options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
