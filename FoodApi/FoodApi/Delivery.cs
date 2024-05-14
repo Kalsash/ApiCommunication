f7b4221fb1f79f5116109c17a10cce49859894c8
Aleksandr Kalinin <akalinin@sfedu.ru>
2024-05-14 10:23:51 +0300

    mediator

---
+++ FoodApi/FoodApi/Delivery.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com
+{
+	[Table("delivery")]
+	public class Delivery
+	{
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Column("order")]
+		public string Order { get; set; }	
+
+	}
+}
