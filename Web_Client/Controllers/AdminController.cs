fd95be6d2d27e0bffc3e7d4d4c69fcd0ac8f312c
Александр Pistak <pistak@deeplay.io>
2024-05-19 16:00:05 +0700

    dfgsdfgsdfg

--- Web_Client/Controllers/AdminController.cs
+++ Web_Client/Controllers/AdminController.cs
@@ -32,6 +32,39 @@ namespace Web_Client.Controllers
                 }
             }
         }
+        //post
+        async public Task<string> AddNewDish(Dish d)
+        {
+            var url = "http://localhost:8080/api/menu";
+            using (HttpClient client = new HttpClient())
+            {
+                //var d = new Dish({Name = "bebra", Price = 1123});
+                string json = JsonConvert.SerializeObject(d);
+                Console.WriteLine(json);
+
+                // Создайте экземпляр HttpContent, если вам нужно отправить данные в теле запроса.
+                // Например, для JSON:
+                // string json = "{\"key\":\"value\"}";
+                // StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+                StringContent content = new StringContent(string.Empty); // Пустое тело запроса
+
+                HttpResponseMessage response = await client.PostAsync(url, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var MenuRespone = await response.Content.ReadAsStringAsync();
+                    return MenuRespone;
+                }
+                else
+                {
+                    return $"Error: {response.StatusCode}";
+                }
+            }
+        }
+
+
+
+
         async public Task<List<Users>> UsersTest()
         {
             var url = "http://localhost:8081/api/users";
