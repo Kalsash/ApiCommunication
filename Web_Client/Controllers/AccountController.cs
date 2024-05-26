c14f0dd24ad23b4ee46fcf6aa594d04ad9084c80
Aleksandr Kalinin <akalinin@sfedu.ru>
2024-05-26 20:06:10 +0300

    user fixes

--- Web_Client/Controllers/AccountController.cs
+++ Web_Client/Controllers/AccountController.cs
@@ -17,19 +17,25 @@ namespace Web_Client.Controllers
             return "ok";
         }
 
-        [HttpPost]
+        // [HttpPost]
+        [HttpGet]
         [Route("account/register")]
-        async public Task<IActionResult> Register()
+        async public Task<string> Register()
         {
-            var user = new Users { Name = "Oscar Montenegro", Phone = "+8 800 555 35 35", Address = "Moscow,Pushkinskaya 89" };
+            var user = new ProUser { Name = "Jocker Montenegro", Password= "Test1*", Phone = "+8 800 555 35 35", Address = "Moscow,Pushkinskaya 89" };
             var url = "http://localhost:8081/account/register";
             using (HttpClient client = new HttpClient())
             {
                 string json = JsonConvert.SerializeObject(user);
                 StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
                 HttpResponseMessage response = await client.PostAsync(url, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    return "DADADAD";
+                }
+                else
+                    return "NENENE";
             }
-            return View();
         }
         //async public Task<ActionResult> MenuAdd(string name, int price)
         //{
---
+++ Web_Client/Models/ProUser.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Web_Client.Models
+{
+    public class ProUser : Users
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
+        public required string Password { get; set; }
+    }
+}
